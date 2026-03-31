a4ece84f5b77e2198a80032111dbb04a3e5d2cbd
Ellasimms1 <500207748@fvtc.edu>
2026-03-31 15:06:43 -0500

    add to my code and explained it

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-(async () => {
-    const getFortune = require('./fourtune')
-    const fortune = getFortune();
+(async () => { 
+    const getFortune = require('./fourtune') // importing the fourtunes.js file 
+    const fortune =  getFortune(); // calling the function to get a fortune 
     console.log(fortune);
 })()
--- fourtune.js
+++ fourtune.js
@@ -1,6 +1,6 @@
-const fortunes =require ('./fortunes.json')
+const fortunes =require ('./fortunes.json') //this is the fortunes array
 
-module.exports = () => {
-    const f = Math.floor(Math.random() * fortunes.length);
+module.exports =  () => { // function  
+    const f = Math.floor(Math.random() * fortunes.length);//this line does the random selection
     return fortunes[f];
 }
